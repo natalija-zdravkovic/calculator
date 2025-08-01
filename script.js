cb9db18e6f2911d2136444fb6fcc2241a48c802d
natalija-zdravkovic <201510014+natalija-zdravkovic@users.noreply.github.com>
2025-08-01 13:42:02 +0200

    Add event listeners for operand and number buttons Add functions with event listeners to handle interactions with numbers and operators

--- script.js
+++ script.js
@@ -11,3 +11,40 @@ const divideButton = document.getElementById("divide")
 const numberButtons = document.querySelectorAll(".number")
 
 const clearButton = document.getElementById("clear")
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", pickNumber)
+})
+
+plusButton.addEventListener("click", function() {
+    operator = "add"
+})
+
+minusButton.addEventListener("click", function() {
+    operator = "substract"
+})
+
+multiplyButton.addEventListener("click", function() {
+    operator = "multiply"
+})
+
+divideButton.addEventListener("click", function() {
+    operator = "divide"
+})
+
+
+function pickNumber(event)
+{
+    const pickedNumber = parseInt(event.target.dataset.value)
+        console.log(pickedNumber)
+        if (number1 === null)
+        {
+            number1 = pickedNumber
+            //dozvoli operande
+        }
+        else
+        {
+            number2 = pickedNumber
+            operate()
+        }
+}
