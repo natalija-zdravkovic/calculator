098191fba62221deb2c4ebee1ba26f44815280e3
natalija-zdravkovic <201510014+natalija-zdravkovic@users.noreply.github.com>
2025-08-01 11:56:06 +0200

    Design calculator logic Designed number and operator handling, called operations on equal sign

---
+++ script.js
@@ -0,0 +1,59 @@
+function opereate(operator, number1, number2)
+{
+    operator(number1, number2)
+}
+
+function calculator()
+{
+    //C je uvek omoguceno i resetuje sve
+    //omoguci samo brojeve
+    //kad se klikne broj on postaje number1
+    //ako se klikne jos jedan broj odmah on postaje druga cifra, ...
+    //omoguci i operande i brojeve
+    //operand postaje operator
+    //kad se klikne broj on postaje number2
+    //pozovi funkciju operate kad se klikne operand ili kad se klikne jednako
+    //ispisi rezultat u konzoli
+    //rezultat postaje number1
+    //ponovi sve opet
+}
+
+function pickOperator()
+{
+
+}
+
+function pickNumber()
+{
+
+}
+
+function unableOperators()
+{
+
+}
+
+function enableOperators()
+{
+
+}
+
+function add()
+{
+
+}
+
+function substract()
+{
+
+}
+
+function multiply()
+{
+
+}
+
+function divide()
+{
+
+}
