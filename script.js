1b6710996636266cd696132d01a27a5940044b08
natalija-zdravkovic <201510014+natalija-zdravkovic@users.noreply.github.com>
2025-08-02 21:58:58 +0200

    Add event listeners for operator buttons Added event listeners to the operator buttons to trigger the operate function on click

--- script.js
+++ script.js
@@ -1,12 +1,67 @@
-//treba da radi i za visecifrene brojeve
-//dodaj back
-//dodaj negativne brojeve 
-//dodaj decimalne brojeve
-//stampa brojeve i rezultat na ekranu
+// treba da radi i za višecifrene brojeve
+// dodaj back
+// dodaj negativne brojeve 
+// dodaj decimalne brojeve
+// štampa brojeve i rezultat na ekranu
 
-function operate()
+let number1 = 0
+let number2 = 0
+let operator = null
+let result = 0
+
+const plusButton = document.getElementById("add")
+const minusButton = document.getElementById("substract")
+const multiplyButton = document.getElementById("multiply")
+const divideButton = document.getElementById("divide")
+const equalButton = document.getElementById("equal")
+
+const numberButtons = document.querySelectorAll(".number")
+const clearButton = document.getElementById("clear")
+const operatorButtons = document.querySelectorAll(".operator")
+
+numberButtons.forEach(button => 
 {
-    switch (operator)
+    button.addEventListener("click", pickNumber)
+})
+
+plusButton.addEventListener("click", function() 
+{
+    operator = "add"
+})
+
+minusButton.addEventListener("click", function() 
+{
+    operator = "substract"
+})
+
+multiplyButton.addEventListener("click", function() 
+{
+    operator = "multiply"
+})
+
+divideButton.addEventListener("click", function() 
+{
+    operator = "divide"
+})
+
+equalButton.addEventListener("click", operate)
+
+clearButton.addEventListener("click", clear)
+
+operatorButtons.forEach(button => 
+    {
+    button.addEventListener("click", function () 
+    {
+        if (operator !== null && number2 !== 0) 
+        {
+            operate()
+        }
+    })
+})
+
+function operate() 
+{
+    switch (operator) 
     {
         case "add":
             result = number1 + number2
@@ -25,78 +80,26 @@ function operate()
     console.log(result)
     number1 = result
     number2 = 0
-    operator = null
-    //blokiraj jednako
+    // blokiraj jednako
 }
 
-let number1 = 0
-let number2 = 0
-let operator = null
-let result = 0
-
-const plusButton = document.getElementById("add")
-const minusButton = document.getElementById("substract")
-const multiplyButton = document.getElementById("multiply")
-const divideButton = document.getElementById("divide")
-const equalButton = document.getElementById("equal")
-
-const numberButtons = document.querySelectorAll(".number")
-
-const clearButton = document.getElementById("clear")
-
-numberButtons.forEach(button => {
-    button.addEventListener("click", pickNumber)
-})
-
-plusButton.addEventListener("click", function() {
-    operator = "add"
-})
-
-minusButton.addEventListener("click", function() {
-    operator = "substract"
-})
-
-multiplyButton.addEventListener("click", function() {
-    operator = "multiply"
-})
-
-divideButton.addEventListener("click", function() {
-    operator = "divide"
-})
-
-equalButton.addEventListener("click", operate)
-
-function disableOperators()
-{
-
-}
-
-disableOperators()
-
-function enableOperators()
-{
-
-}
-
-function pickNumber(event)
+function pickNumber(event) 
 {
     const pickedNumber = parseInt(event.target.dataset.value)
-        console.log(pickedNumber)
-        if (operator === null)
+    console.log(pickedNumber)
+
+    if (operator === null) 
         {
-            number1 = number1*10 + pickedNumber
-            //dozvoli operatore
-        }
-        else
-        {
-            number2 = number2*10 + pickedNumber
-            //dozvoli jednako
-        }
+        number1 = number1 * 10 + pickedNumber
+        // dozvoli operatore
+    } else 
+    {
+        number2 = number2 * 10 + pickedNumber
+        // dozvoli jednako
+    }
 }
 
-
-
-function clear()
+function clear() 
 {
     number1 = 0
     number2 = 0
@@ -104,4 +107,12 @@ function clear()
     result = null
 }
 
-clearButton.addEventListener("click", clear)
+function disableOperators() 
+{
+}
+
+function enableOperators() 
+{
+}
+
+
